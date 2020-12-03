f0abc1d14b96472d35696f94b9c011763d9b9386
Nicolas <aponcenicolas@gmail.com>
2020-12-03 14:14:03 -0500

    union de colecciones con flatmap

--- functional-programming-java/src/main/java/lux/pe/na/course/FunctionalProgrammingJavaApplication.java
+++ functional-programming-java/src/main/java/lux/pe/na/course/FunctionalProgrammingJavaApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class FunctionalProgrammingJavaApplication implements CommandLineRunner {
@@ -16,5 +19,14 @@ public class FunctionalProgrammingJavaApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        List<String> user1 = Arrays.asList("nicolas", "dyana");
+        List<String> user2 = Arrays.asList("maria", "pedro");
+        List<String> user3 = Arrays.asList("juan", "bertha");
+
+        List<String> result = Stream.of(user1, user2, user3)
+                .flatMap(value -> value.stream())
+                .collect(Collectors.toList());
+
+        System.out.println(result);
     }
 }
