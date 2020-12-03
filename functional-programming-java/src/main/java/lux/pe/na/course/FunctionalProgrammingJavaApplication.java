7963174379008df52277acfc8c22bafe4d918575
Nicolas <aponcenicolas@gmail.com>
2020-12-02 21:41:29 -0500

    base code

---
+++ functional-programming-java/src/main/java/lux/pe/na/course/FunctionalProgrammingJavaApplication.java
@@ -0,0 +1,20 @@
+package lux.pe.na.course;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+public class FunctionalProgrammingJavaApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionalProgrammingJavaApplication.class, args);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+    }
+}
