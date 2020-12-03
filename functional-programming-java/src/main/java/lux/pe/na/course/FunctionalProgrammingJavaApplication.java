9f6d5d7f4e6a0827728e05e8c464e0e6aed64f36
Nicolas <aponcenicolas@gmail.com>
2020-12-03 14:02:49 -0500

    transformacion de datos

--- functional-programming-java/src/main/java/lux/pe/na/course/FunctionalProgrammingJavaApplication.java
+++ functional-programming-java/src/main/java/lux/pe/na/course/FunctionalProgrammingJavaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class FunctionalProgrammingJavaApplication implements CommandLineRunner {
@@ -16,5 +17,10 @@ public class FunctionalProgrammingJavaApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        List<Integer> result = numbers.stream()
+                .map(number -> number * number)
+                .collect(Collectors.toList());
+        System.out.println(result);
     }
 }
