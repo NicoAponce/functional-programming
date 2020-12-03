b0d7b14f35f6b0892e72732d2da792db1d52dce2
Nicolas <aponcenicolas@gmail.com>
2020-12-03 13:12:20 -0500

    programacion imperativa y declarativa

--- functional-programming-java/src/main/java/lux/pe/na/course/FunctionalProgrammingJavaApplication.java
+++ functional-programming-java/src/main/java/lux/pe/na/course/FunctionalProgrammingJavaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,5 +17,27 @@ public class FunctionalProgrammingJavaApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        List<User> users = new ArrayList<>();
+        users.add(new User("nicolas", 16));
+        users.add(new User("dyana", 17));
+        users.add(new User("luis", 18));
+        users.add(new User("carlos", 19));
+        users.add(new User("maria", 20));
+        users.add(new User("maria", 20));
+        users.add(new User("maria", 20));
+
+        //TODO: Imperative
+        int count = 0;
+        for (User user : users) {
+            if (user.getAge() > 18) {
+                count++;
+            }
+        }
+        System.out.println("users over 18 years old are " + count);
+
+
+        //TODO: Declarative
+        count= (int) users.stream().filter(user -> user.getAge()>18).count();
+        System.out.println(count);
     }
 }
